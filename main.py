e63c3594345c37feb283c602e3c692a8baaa2326
Ilyes Kechidi <ilyes.spd@gmail.com>
2016-11-10 18:26:01 +0100

    added main ui file

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+import sys
+from PyQt5.QtWidgets import QApplication,QWidget
+from ui_main import Ui_Form
+
+
+class MainWidget(QWidget):
+    def __init__(self):
+        super(MainWidget, self).__init__()
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
 
 
 def main():
-	print("Netcut -- Version : 0.1")
+    app = QApplication(sys.argv)
+
+    w = MainWidget()
+
+    w.show()
+    sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
-	main()
+    main()
---
+++ ui_main.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ui/main.ui'
+#
+# Created by: PyQt5 UI code generator 5.5.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(523, 376)
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(Form)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.btn_refresh = QtWidgets.QPushButton(Form)
+        self.btn_refresh.setObjectName("btn_refresh")
+        self.horizontalLayout.addWidget(self.btn_refresh)
+        self.pushButton_2 = QtWidgets.QPushButton(Form)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.horizontalLayout.addWidget(self.pushButton_2)
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setObjectName("pushButton")
+        self.horizontalLayout.addWidget(self.pushButton)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.tbl_list = QtWidgets.QTableWidget(Form)
+        self.tbl_list.setObjectName("tbl_list")
+        self.tbl_list.setColumnCount(0)
+        self.tbl_list.setRowCount(0)
+        self.verticalLayout.addWidget(self.tbl_list)
+        self.verticalLayout_2.addLayout(self.verticalLayout)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "NetShut"))
+        self.btn_refresh.setText(_translate("Form", "Refresh"))
+        self.pushButton_2.setText(_translate("Form", "PushButton"))
+        self.pushButton.setText(_translate("Form", "PushButton"))
+
